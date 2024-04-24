655871273549138b46d89f354cd95884358546bd
Ananya Verma <165065804+PES2UG23EC017@users.noreply.github.com>
2024-04-24 21:50:49 +0530

    Add files via upload

---
+++ Grade Card 1/makefile.mk
@@ -0,0 +1,18 @@
+CC= gcc
+CFLAGS= -Wall -Wextra
+
+EXECUTABLE= gradecard
+
+all: $(EXECUTABLE)
+
+$(EXECUTABLE): main.o grades.o
+	$(CC) $(CFLAGS) -o $(EXECUTABLE) main.o grades.o
+
+main.o: main.c grades.h
+	$(CC) $(CFLAGS) -c main.c
+
+grades.o: grades.c grades.h
+	$(CC) $(CFLAGS) -c grades.c
+
+clean:
+	rm -f $(EXECUTABLE) main.o grades.o
